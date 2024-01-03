084d435f644e323da14872bc1d3a8e49a02870a4
Anjali Sharma <122967990+ItsAnjaliSharma@users.noreply.github.com>
2024-01-03 19:33:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+setInterval(()=>{
+     //pass the date time minutes and seconds through methods
+   d=new Date();
+   htime=d.getHours();
+   mtime=d.getMinutes();
+   stime=d.getSeconds();
+   //logic
+   hrotation=30*htime+mtime/2;
+   mrotation=6*mtime;
+   srotation=6*stime;
+   hour.style.transform=`rotate(${hrotation}deg)`;
+   minute.style.transform=`rotate(${mrotation}deg)`;
+   second.style.transform=`rotate(${srotation}deg)`;
+}, 1000);
